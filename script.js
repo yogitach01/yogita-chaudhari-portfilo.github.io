cb66b96fb468ed3394806379d390165ff4fc6aae
yogita pandurang chaudhari <80738479+yogitach01@users.noreply.github.com>
2021-07-18 11:41:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){var typed = new Typed(".typing-2", {
+        strings: ["Full Stack Developer", "Competitive Programmer", "Open Source Contributor", "Aspiring Software Developer", "Android App developer","content writer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
